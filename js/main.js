55c516d8ccc8f60dbeef54e8c7b275fdb0950400
DjamalMatua <48605394+DjamalMatua@users.noreply.github.com>
2019-09-03 22:28:00 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -19,3 +19,14 @@ $(document).ready(function () {
     })
 });
 //////
+$('.over-numb').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter:$(this).text()
+    },{
+        duration: 4000,
+        easing: 'swing',
+        step:function(now){
+        $(this).text(Math.ceil(now));
+    }
+    });
+});
